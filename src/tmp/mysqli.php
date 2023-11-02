f9579a0ce8e219a105aef1e617b68d0ad7410722
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-11-02 23:09:20 +0900

    php sql insert and error check

--- src/tmp/mysqli.php
+++ src/tmp/mysqli.php
@@ -9,5 +9,28 @@ if (!$link) {
 }
 
 echo 'データベースに接続できました。' . PHP_EOL;
+
+
+$sql = <<<EOT
+INSERT INTO companies (
+    name,
+    establishment_date,
+    founder
+) VALUES (
+    'SmartHR Inc.',
+    '2013-01-23',
+    'Shoji Miyata'
+)
+EOT;
+
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    echo 'データを追加しました' . PHP_EOL;
+} else {
+    echo 'Error: データの追加に失敗しました' . PHP_EOL;
+    echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+}
+
 mysqli_close($link);
 echo 'データベースとの接続を切断しました。' . PHP_EOL;
