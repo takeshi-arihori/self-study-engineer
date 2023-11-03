ac668fd7441b97e825c8d2d1bfbbd4c4eb822e64
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-11-03 23:08:02 +0900

    select

--- src/book_log.php
+++ src/book_log.php
@@ -1,41 +1,100 @@
 <?php
 
+// DBからリストを取得する
+function listReviews($link)
+{
+    echo '登録されている読書ログを表示します' . PHP_EOL;
+
+    // DBから取得
+    $sql = 'SELECT id, title, author, status, score, summary FROM reviews';
+    $results = mysqli_query($link, $sql);
+
+
+    while ($review = mysqli_fetch_assoc($results)) {
+        echo '書籍名 : ' . $review['title'] . PHP_EOL;
+        echo '著者名 : ' . $review['author'] . PHP_EOL;
+        echo '読書状況 : ' . $review['status'] . PHP_EOL;
+        echo '評価 : ' . $review['score'] . PHP_EOL;
+        echo '感想 : ' . $review['summary'] . PHP_EOL;
+        echo '----------------------' . PHP_EOL;
+    }
+
+    mysqli_free_result($results);
+}
+
 function dbConnect()
 {
     $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
-
-    if (!$link) {
-        echo 'データベースに接続できませんでした' . PHP_EOL;
-        echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
-    echo 'データベースに接続できました。' . PHP_EOL;
-
     return $link;
+}
+
+function validate($review)
+{
+    $errors = []; // エラー内容を格納する配列
+
+    // 書籍名が正しく入力されているかチェック
+    if (!strlen($review['title'])) {
+        $errors['title'] = '書籍名を入力してください';
+    } else if (strlen($review['title']) > 255) {
+        $errors['title'] = '書籍名は255文字以内で入力してください';
+    }
+
+    // 著者名(author)が正しく入力されているかチェック
+    if (!strlen($review['author'])) {
+        $errors['author'] = '著者名を入力してください';
+    }
+
+    // 読書状況(status)が正しく入力されているかチェック
+    if (!in_array($review['status'], ['未読', '読んでる', '読了'])) {
+        $errors['status'] = '読書状況は未読、読んでる、読了のいずれかを入力してください';
+    }
+
+    // 評価の数値が1以上5以下であるかチェック
+    if ($review['score'] < 1 || $review['score'] > 5) {
+        $errors['score'] = '評価は1〜5の整数を入力してください';
+    }
+
+    // 感想(summary)が正しく入力されているかチェック
+    if (!strlen($review['summary'])) {
+        $errors['summary'] = '感想を入力してください';
+    } else if (strlen($review['summary']) > 1000) {
+        $errors['summary'] = '感想は1000文字以内で入力してください';
+    }
+
+    return $errors;
 }
 
 function createReview($link)
 {
+    $review = [];
+
     echo PHP_EOL;
     echo '読書ログを登録します' . PHP_EOL;
 
     echo '書籍名:';
-    $title = trim(fgets(STDIN));
+    $review['title'] = trim(fgets(STDIN));
 
     echo '著者名:';
-    $author = trim(fgets(STDIN));
+    $review['author'] = trim(fgets(STDIN));
 
     echo '読書状況（未読,読んでる,読了）:';
-    $status = trim(fgets(STDIN));
+    $review['status'] = trim(fgets(STDIN));
 
     echo '評価（5点満点の整数）:';
-    $score = (int) trim(fgets(STDIN));
+    $review['score'] = (int) trim(fgets(STDIN));
 
     echo '感想 :';
-    $summary = trim(fgets(STDIN));
+    $review['summary'] = trim(fgets(STDIN));
 
-    echo ' 登録が完了しました' . PHP_EOL . PHP_EOL;
-    echo PHP_EOL;
+    // エラーチェック
+    $validated = validate($review);
+    // エラーがあれば表示して終了
+    if (count($validated) > 0) {
+        foreach ($validated as $error) {
+            echo $error . PHP_EOL;
+        }
+        return;
+    }
 
     $sql = <<<EOT
 INSERT INTO reviews (
@@ -45,11 +104,11 @@ INSERT INTO reviews (
     score,
     summary
 ) VALUES (
-    "{$title}",
-    "{$author}",
-    "{$status}",
-    $score,
-    "{$summary}"
+    "{$review['title']}",
+    "{$review['author']}",
+    "{$review['status']}",
+    "{$review['score']}",
+    "{$review['summary']}"
 )
 EOT;
 
@@ -63,25 +122,7 @@ EOT;
     }
 }
 
-
-function listReviews($reviews)
-{
-    echo PHP_EOL;
-    // 読書ログを表示する
-    echo '登録されている読書ログを表示します' . PHP_EOL;
-
-    foreach ($reviews as $review) {
-        echo '書籍名:' . $review['title'] . PHP_EOL;
-        echo '著者名:' . $review['author'] . PHP_EOL;
-        echo '読書状況（未読,読んでる,読了）:' . $review['status'] . PHP_EOL;
-        echo '評価（5点満点の整数）:' . $review['score'] . PHP_EOL;
-        echo '感想 :' . $review['summary'] . PHP_EOL;
-        echo '-------------' . PHP_EOL;
-    }
-    echo PHP_EOL;
-}
-
-
+$reviews = [];
 $link = dbConnect();
 
 while (true) {
@@ -91,16 +132,11 @@ while (true) {
     echo '番号を選択してください(1,2,9) : ';
 
     $num = trim(fgets(STDIN));
-    // 大文字と小文字を区別しない
-    $num = strtolower($num);
-    // 全角と半角両方受け入れる
-    $num = mb_convert_kana($num, 'a', 'UTF-8');
-
 
     if ($num === '1') {
         createReview($link);
     } else if ($num === '2') {
-        listReviews($reviews);
+        listReviews($link);
     } else if ($num === '9') {
         // アプリケーションを終了する
         mysqli_close($link);
