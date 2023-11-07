38627e0f485d2d056559613dc9fda2b4d380e6dc
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-11-07 20:41:49 +0900

    List display

--- src/views/index.php
+++ src/views/index.php
@@ -1,17 +1,19 @@
-<a href="new.php">読書ログを登録する</a>
+<a href="new.php" class="btn btn-primary mb-4">読書ログを登録する</a>
 <main>
     <?php if (count($reviews) > 0) : ?>
         <?php foreach ($reviews as $review) : ?>
-            <section>
-                <h2><?php echo escape($review['title']) ?></h2>
-                <div>
-                    <?php echo escape($review['author']) ?>&nbsp;/&nbsp;
-                    <?php echo escape($review['status']) ?>&nbsp;/&nbsp;
-                    <?php echo escape($review['score']) ?>&nbsp;/&nbsp;
+            <section class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h2 class="card-title h4 text-dark mb-3"><?php echo escape($review['title']) ?></h2>
+                    <div class="small mb-3">
+                        <?php echo escape($review['author']) ?>&nbsp;/&nbsp;
+                        <?php echo escape($review['status']) ?>&nbsp;/&nbsp;
+                        <?php echo escape($review['score']) ?>点
+                    </div>
+                    <p>
+                        <?php echo nl2br(escape($review['summary']), false) ?>
+                    </p>
                 </div>
-                <p>
-                    <?php echo nl2br(escape($review['summary']), false) ?>
-                </p>
             </section>
         <?php endforeach; ?>
     <?php else : ?>
